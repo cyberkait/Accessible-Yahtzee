853f2b6ec55454895cd2f11a5fe09d3ae57f9434
cyberkait <kaitlenius@gmail.com>
2023-12-12 20:50:58 -0600

    Linted up the code and fixed the chance tile.

--- scoreboard.py
+++ scoreboard.py
@@ -1,31 +1,53 @@
-from os import name
+"""
+This is the module which contains scoreboard information.
+"""
+
 import tiles as t
 
 
-all_tiles = [t.SumTile("ones", 1), t.SumTile("twos", 2), t.SumTile("threes", 3), t.SumTile("fours", 4), t.SumTile("fives", 5), t.SumTile("sixes", 6), t.NumberTile("three of a kind", 3), t.NumberTile("four of a kind", 4), t.FullHouse(), t.SmallStraight(), t.LargeStraight(), t.NumberTile("yahtzee", 5), t.SumTile("chance", 0)]
+all_tiles = [
+    t.SumTile("ones", 1),
+    t.SumTile("twos", 2),
+    t.SumTile("threes", 3),
+    t.SumTile("fours", 4),
+    t.SumTile("fives", 5),
+    t.SumTile("sixes", 6),
+    t.NumberTile("three of a kind", 3),
+    t.NumberTile("four of a kind", 4),
+    t.FullHouse(),
+    t.SmallStraight(),
+    t.LargeStraight(),
+    t.NumberTile("yahtzee", 5),
+    t.SumTile("chance", 0),
+]
+
 
 class Scoreboard:
     """
     Representation of the game's scoreboard.
     """
+
     def __init__(self, contents):
         """
         Initializes scoreboard.
         """
         self.contents = contents
         self.total_score = 0
+
     def get_score(self, ind, die_list):
         """
         Gets the score of a tile at a certain index.
         """
         s = self.contents[ind].score_tile(die_list)
         return s
+
     def view_score(self, num, lst):
         """
         Views the score of a tile.
         """
         score = self.get_score(num, lst)
         print(f"{self.contents[num].name}: {score}")
+
     def lock_score(self, num, lst):
         """
         Locks the score for a tile and adds it to both the tile and board.
@@ -34,3 +56,11 @@ class Scoreboard:
         self.contents[num].score = s
         self.contents[num].hasScore = True
         self.total_score += s
+
+
+sb = Scoreboard(all_tiles)
+dice = [1, 2, 3, 4, 5]
+count = 0
+for t in sb.contents:
+    sb.view_score(count, dice)
+    count += 1
--- tiles.py
+++ tiles.py
@@ -1,7 +1,12 @@
+"""
+Module which contains all of the tiles used for Yahtzee.
+"""
+
 class Tile:
     """
     Base class for Yahtzee tiles.
     """
+
     def __init__(self, name):
         """
         Initializes the tile.
@@ -9,8 +14,10 @@ class Tile:
         self.name = name
         self.score = 0
         self.hasScore = False
+
     def __str__(self):
         return self.name
+
     def finalize_score(self, value):
         """
         Scores tile and then locks it.
@@ -23,39 +30,46 @@ class SumTile(Tile):
     """
     Tile in which all numbers are added up to create the score.
     """
+
     def __init__(self, name, number):
         """
         Initializes the tile.
         """
         super().__init__(name)
         self.number = number
+
     def score_tile(self, dice):
         """
         Scores the tile.
         """
         if self.hasScore == False:
             new_score = self.score
-            for die in dice:
-                if die == self.number:
-                    new_score += die
+            if self.number == 0:
+                new_score = sum(dice)
+            else:
+                for die in dice:
+                    if die == self.number:
+                        new_score += die
             return new_score
 
 
 class NumberTile(Tile):
-    """ 
+    """
     Tiles which check for more than 3 of a number.
-    """ 
+    """
+
     def __init__(self, name, numberOf):
         super().__init__(name)
         self.numberOf = numberOf
+
     def score_tile(self, dice):
         """
         Scores tile.
         """
         if self.hasScore == False:
             dice.sort()
-            for index in range(len(dice)-self.numberOf+1):
-                if len(set(dice[index:index+self.numberOf])) == 1:
+            for index in range(len(dice) - self.numberOf + 1):
+                if len(set(dice[index : index + self.numberOf])) == 1:
                     if len(set(dice)) == 1:
                         return 50
                     else:
@@ -67,20 +81,28 @@ class FullHouse(Tile):
     """
     Full house tile.
     """
+
     def __init__(self):
         """
         Initializes tile.
         """
         super().__init__(name="full house")
-        self.name = name
+
     def score_tile(self, dice):
         """
         Scores the tile.
         """
         if self.hasScore == False:
             dice.sort()
-            if ((dice[0] == dice[1] and dice[1] != dice[2] and dice[2] == dice[3] == dice[4]) or
-                (dice[0] == dice[1] == dice[2] and dice[2] != dice[3] and dice[3] == dice[4])):
+            if (
+                dice[0] == dice[1]
+                and dice[1] != dice[2]
+                and dice[2] == dice[3] == dice[4]
+            ) or (
+                dice[0] == dice[1] == dice[2]
+                and dice[2] != dice[3]
+                and dice[3] == dice[4]
+            ):
                 return 25
             return 0
 
@@ -89,11 +111,13 @@ class SmallStraight(Tile):
     """
     Small straight tile.
     """
+
     def __init__(self):
         """
         Initializes tile.
         """
         super().__init__(name="small straight")
+
     def score_tile(self, dice):
         """
         Scores the tile.
@@ -101,7 +125,12 @@ class SmallStraight(Tile):
         if self.hasScore == False:
             dice = list(set(dice))
             dice.sort()
-            if len(dice) >= 4 and (dice[:4] == [1, 2, 3, 4] or dice[:4] == [2, 3, 4, 5] or dice[1:] == [3, 4, 5, 6]or dice[:4] == [3, 4, 5, 6] ):
+            if len(dice) >= 4 and (
+                dice[:4] == [1, 2, 3, 4]
+                or dice[:4] == [2, 3, 4, 5]
+                or dice[1:] == [3, 4, 5, 6]
+                or dice[:4] == [3, 4, 5, 6]
+            ):
                 return 30
             return 0
 
@@ -110,12 +139,13 @@ class LargeStraight(Tile):
     """
     Large straight tile.
     """
+
     def __init__(self):
         """
         Initializes tile.
         """
         super().__init__(name="large straight")
-        self.name = name
+
     def score_tile(self, dice):
         """
         Scores the tile.
